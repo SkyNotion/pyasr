4915450ff27b2b0898290d3e1443e96f1b99797a
SkyNotion <meshdrew6@gmail.com>
2026-05-23 10:11:51 +0100

    Added cleaner and general improvements

--- main.py
+++ main.py
@@ -39,8 +39,41 @@ OPENAI_API = "openai.api"
 
 http_client = httpx.Client(timeout=500000)
 
+CLEANER_PROMPT = '''\
+You are an expert prompt engineer. Your task is to take messy, raw ASR (speech-to-text) transcripts and convert them into clear, highly structured instructions for an AI coding agent.
+Rules:
+1. Remove all conversational filler (um, ah, okay).
+2. Fix obvious phonetic transcription errors.
+3. Structure the output with clear headings: "Context", "Current State", and a numbered list for "Tasks".
+4. Do not add any new technical requirements that were not implied in the original text.
+5. Output ONLY the cleaned prompt. Do not include conversational replies like "Here is your text."'''
+
 def gen_short_uuid(prefix = ""):
     return f"{prefix}{str(uuid.uuid4()).split("-")[4]}"
+
+def llm_cleaner(text):
+    response = http_client.post(
+        url="http://localhost:11434/api/generate",
+        json={
+            "model": "qwen3:0.6b",
+            "system": CLEANER_PROMPT,
+            "prompt": text,
+            "stream": False,
+            "think": True,
+            "keep_alive": "1h",
+            "options": {
+                "temperature": 0.7,
+                "top_k": 20,
+                "top_p": 0.8,
+                "min_p": 0,
+            }
+        }
+    )
+
+    response = response.json()
+    response = response["response"]
+    response = [x.strip() for x in response.splitlines() if len(x) > 0]
+    return "\n".join(response)
 
 def inference(file_path, engine):
     if engine == WHISPER_CPP:
@@ -64,11 +97,11 @@ def inference(file_path, engine):
         response = http_client.post(
             url="http://127.0.0.1:9953/v1/audio/transcriptions",
             data={
-                "prompt": "Transcribe to text",
+                "prompt": "language English<asr_text>",
                 "model": "Qwen3-ASR",
                 "response_format": "json",
                 "language": "English",
-                "temperature": "0.01",
+                "temperature": "0.000001",
             },
             files={
                 "file": open(file_path, "rb")
@@ -83,26 +116,34 @@ def inference(file_path, engine):
 def audio_callback(indata, frames, time, status):
     META["buffer"].append(indata.copy())
 
-def run_audio_stream():
-    with sd.InputStream(samplerate=SAMPLE_RATE, channels=CHANNELS, latency="high",
-                blocksize=BLOCK_SIZE, callback=audio_callback, device=DEVICE):
-        print("run_audio_stream - Started recording")
-        META["event"].wait(timeout=None)
-        print("run_audio_stream - Stopping recording")
-        META["event"].clear()
+def run_audio_stream(file_path = None):
+    if file_path is None:
+        with sd.InputStream(samplerate=SAMPLE_RATE, channels=CHANNELS, latency="high",
+                    blocksize=BLOCK_SIZE, callback=audio_callback, device=DEVICE):
+            print("run_audio_stream - Started recording")
+            META["event"].wait(timeout=None)
+            print("run_audio_stream - Stopping recording")
+            META["event"].clear()
 
-    recording = np.concatenate(META["buffer"], axis=0)
-    file_path = f"{AUDIO_ROOT}/{gen_short_uuid("rec")}.wav"
-    print(f"run_audio_stream - Saving file to {file_path}")
-    write(file_path, SAMPLE_RATE, recording)
-    print(f"run_audio_stream - Saved file")
+            recording = np.concatenate(META["buffer"], axis=0)
+        file_path = f"{AUDIO_ROOT}/{gen_short_uuid("rec")}.wav"
+        print(f"run_audio_stream - Saving file to {file_path}")
+        write(file_path, SAMPLE_RATE, recording)
+        print(f"run_audio_stream - Saved file")
     print(f"run_audio_stream - Transcribing...")
 
     start_time = time()
+
     response = inference(file_path, OPENAI_API)
 
     print("")
-    print(f"{TERM_BLUE}{response}{TERM_RESET}")
+    print(f"{TERM_BLUE}Raw\n----------\n{response}{TERM_RESET}")
+    print("")
+
+    response = llm_cleaner(response)
+
+    print("")
+    print(f"{TERM_BLUE}Cleaned\n----------\n{response}{TERM_RESET}")
     print("")
 
     print(f"run_audio_stream - Transcribed in {round(time() - start_time, 2)}s")
